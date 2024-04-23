f0285c15ad686b648555a2ced1f4f1bd650d20ef
Brian Ho <YWHo@users.noreply.github.com>
2024-04-24 00:19:36 +1200

    setup to enable Progressive Web App

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  disable: false,
+  reloadOnOnline: true,
+  // swcMinify: true,  // Note: this option no longer exist
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
